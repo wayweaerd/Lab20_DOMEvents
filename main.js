25337e79dcd4db1ae63023c10d085e279e99e21d
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-03-25 11:03:27 +0300

    Step 7: mini task with DOM events

--- main.js
+++ main.js
@@ -36,3 +36,13 @@ button.addEventListener("click", () => {
     output.style.color = "red";
     }
 });
+
+
+
+const clearButton = document.getElementById("clearButton");
+clearButton.addEventListener("click", () => {
+    input.value = "";
+    output.textContent = "";
+});
+output.style.color = "";
+input.focus();
