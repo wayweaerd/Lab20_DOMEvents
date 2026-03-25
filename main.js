283386c899b3609276e0c21b9a045fd749e7c9ff
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-03-25 11:10:23 +0300

    Step 8: basic HTML form handling

--- main.js
+++ main.js
@@ -46,3 +46,15 @@ clearButton.addEventListener("click", () => {
 });
 output.style.color = "";
 input.focus();
+
+const form = document.getElementById("userForm");
+const usernameInput = document.getElementById("username");
+const ageInput = document.getElementById("age");
+const result = document.getElementById("formResult");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const name = usernameInput.value;
+    const age = ageInput.value;
+    result.textContent = `Имя: ${name}, Возраст: ${age}`;
+});
